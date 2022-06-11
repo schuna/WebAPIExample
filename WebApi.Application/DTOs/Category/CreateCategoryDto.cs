5309bfe254d37a0724dea82ac8eff97dbaa40a21
schuna <jiyong1972@gmail.com>
2022-06-12 07:06:49 +0900

    add Authorization

--- WebApi.Application/DTOs/Category/CreateCategoryDto.cs
+++ WebApi.Application/DTOs/Category/CreateCategoryDto.cs
@@ -6,6 +6,5 @@ namespace WebApi.Application.DTOs.Category
     public class CreateCategoryDto : ICategoryDto
     {
         public string? Name { get; set; }
-        public virtual List<ProductDto>? Products { get; set; }
     }
 }
